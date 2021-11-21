02f82d0a651bed2a4c19282990d4e308e9131f22
daleepsra <axelsra31@gmail.com>
2021-11-21 20:08:53 +0530

    project

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"sefigas@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+enableXsrfProtection=false\n
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
